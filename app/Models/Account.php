b1ed6f1a275c2cf926530180251731db45096670
Ilker Mutlu <ilker.mutlu@gmail.com>
2023-08-18 16:42:22 +0100

    Remove unused methods

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,43 +2,15 @@
 
 namespace App\Models;
 
-use App\Events\AccountCreated;
-use App\Events\MoneyAdded;
-use App\Events\MoneySubtracted;
-use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\EventSourcing\Projections\Projection;
-use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent;
 
 class Account extends Projection
 {
     protected $guarded = [];
 
-    public static function createWithAttributes(array $attributes): Account
-    {
-        $attributes['uuid'] = (string) Uuid::uuid4();
-
-        event(new AccountCreated($attributes));
-
-        return static::uuid($attributes['uuid']);
-    }
-
-    public function addMoney(int $amount)
-    {
-        event(new MoneyAdded($this->uuid, $amount));
-    }
-
-    public function subtractMoney(int $amount)
-    {
-        event(new MoneySubtracted($this->uuid, $amount));
-    }
-
-    public function transactions()
+    public function transactions(): HasMany
     {
         return $this->hasMany(Transaction::class, 'account_id', 'id');
-    }
-
-    public function storedEvents()
-    {
-        return $this->hasMany(EloquentStoredEvent::class, 'aggregate_uuid');
     }
 }
